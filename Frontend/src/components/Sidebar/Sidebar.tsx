fc60830a3bce29fe686ed7abb549b1fe82308958
Etativel <farhanmaulana4611@gmail.com>
2025-06-02 17:11:55 +0800

    feat: add map preview button

--- Frontend/src/components/Sidebar/Sidebar.tsx
+++ Frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,22 @@
-import { Scan } from "lucide-react";
+import { Scan, Satellite, Map } from "lucide-react";
 
-export default function Sidebar() {
+interface SidebarProps {
+  satellite: boolean;
+  setSatellite: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Sidebar({ setSatellite, satellite }: SidebarProps) {
   return (
-    <div className="absolute left-0 top-0 w-10 h-full flex flex-col items-center border-transparent py-5 bg-neutral-800">
+    <div className="absolute left-0 top-0 w-10 h-full flex flex-col items-center border-transparent py-5 gap-5 bg-neutral-800">
       <button>
         <Scan className="text-neutral-400" />
+      </button>
+      <button onClick={() => setSatellite((prev) => !prev)}>
+        {satellite ? (
+          <Map className="text-neutral-400" />
+        ) : (
+          <Satellite className="text-neutral-400" />
+        )}
       </button>
     </div>
   );
