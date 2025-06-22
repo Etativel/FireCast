b2d24075936af38033bd7f1bae319131a7ff7950
Etativel <farhanmaulana4611@gmail.com>
2025-06-22 13:44:50 +0700

    feat: prediction route

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,13 +5,20 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Layout from "./Layout.tsx";
 import WeatherInfo from "./pages/WeatherInfo/WeatherInfo.tsx";
+import PredictionPage from "./pages/prediction/PredictionPage.tsx";
+import NotFound from "./pages/NotFound/NotFound.tsx";
 
 const router = createBrowserRouter([
+  {
+    path: "/prediction",
+    element: <PredictionPage />,
+  },
   {
     path: "/",
     element: <Layout />,
   },
   { path: "/weather-info", element: <WeatherInfo /> },
+  { path: "*", element: <NotFound /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
